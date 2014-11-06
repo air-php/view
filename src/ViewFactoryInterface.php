7e9edb5cab51e3988e85c91d6af64ee6cff6d3ad
Mark Locker <mark.locker@easyfundraising.org.uk>
2014-11-06 13:33:56 +0000

    Added methods missing from the interface.

--- src/ViewFactoryInterface.php
+++ src/ViewFactoryInterface.php
@@ -4,5 +4,14 @@ namespace Air\View;
 
 interface ViewFactoryInterface
 {
+    /**
+     * @param string $fileName The name of the file to load.
+     * @return ViewInterface A view.
+     */
     public function get($fileName);
+
+    /**
+     * @param array $viewPaths An array of paths to look for view files in.
+     */
+    public function registerPaths(array $viewPaths);
 }
